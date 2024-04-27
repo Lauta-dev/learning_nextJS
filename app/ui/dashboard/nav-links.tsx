502bec2f9a9976f6f6486cd3971b7dc4ec59793b
lauta <altairdiaz245@gmail.com>
2024-04-26 22:34:09 -0300

    complete: chapter 5

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,44 +1,51 @@
 'use client'
 
 import {
-UserGroupIcon,
-HomeIcon,
-DocumentDuplicateIcon,
+  UserGroupIcon,
+  HomeIcon,
+  DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
 
-// Link es una "extencion" o "mejora" de <a />
+// Link es una "extencion" o "mejora" de <a/>
 // NextJS por debajo hace un "prefeching" del código el enrutado por link
 import Link from 'next/link';
-import { usePathname } from 'next/navigation'; // Hook que obtiene el path actual del usuario
+import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
-{ name: 'Home', href: '/dashboard', icon: HomeIcon },
-{
-name: 'Invoices',
-href: '/dashboard/invoices',
-icon: DocumentDuplicateIcon,
-},
-{ name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'Home', href: '/dashboard', icon: HomeIcon },
+  {
+    name: 'Invoices',
+    href: '/dashboard/invoices',
+    icon: DocumentDuplicateIcon,
+  },
+  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: "Tiki", href: '/dashboard/tiki' }
 ];
 
 export default function NavLinks() {
-const pathname = usePathname();
-return (
-<>
-  {links.map((link) => {
-  const LinkIcon = link.icon;
+  const pathname = usePathname();
   return (
-  <Link key={link.name} href={link.href}
-    className={clsx( 'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3'
-    , { 'bg-sky-100 text-blue-600' : pathname===link.href, }, )}>
-  <LinkIcon className="w-6" />
-  <p className="hidden md:block">{link.name}</p>
-  </Link>
+    <>
+      {links.map((link) => {
+        const LinkIcon = link.icon;
+        return (
+          <Link
+            key={link.name}
+            href={link.href}
+                       className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              {
+                'bg-sky-100 text-blue-600': pathname === link.href,
+              },
+            )}
+          >
+            <p className="hidden md:block">{link.name}</p>
+          </Link>
+        );
+      })}
+    </>
   );
-  })}
-</>
-);
 }
